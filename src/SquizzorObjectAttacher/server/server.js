f4582890501249b09b90c6bb6e28d12c2c797d66
Andy <mail@ak95.de>
2022-03-23 18:35:25 +0100

    add

---
+++ src/SquizzorObjectAttacher/server/server.js
@@ -0,0 +1,10 @@
+﻿import * as alt from 'alt-server';
+
+
+alt.onClient('objectAttacher:attachedObject', (player, objectName) => {
+    player.setSyncedMeta('AttachedObject', objectName);
+});
+
+alt.onClient('objectAttacher:detachedObject', (player) => {
+    player.setSyncedMeta('AttachedObject', null);
+});
